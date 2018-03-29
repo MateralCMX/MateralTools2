2ebf2c254a57ed9746b27d632d9c3deb1011b052
陈明旭 <cloomcmx1554@hotmail.com>
2018-03-29 13:37:19 +0800

    添加项目文件。

---
+++ MateralTools.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MateralTools.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MateralTools.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MateralTools.API.WebApiApplication" Language="C#" %>
